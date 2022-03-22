369de48012f344f6be7c95e7d48c25d2e7a6fea9
Junfan-clark <488591831@qq.com>
2022-03-22 15:24:38 +0000

    1.0 开发了计算器的基础版本

---
+++ calculator/1.0 (2022.3.14)/multiplication.cpp
@@ -0,0 +1,16 @@
+// num1 * num2 (integer)
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int num1,num2;
+	
+	cout << "Please enter two numbers to multiplication: " << endl;
+	cin >> num1 >> num2;	// It takes two lines.
+	cout << "It equal to " << num1 * num2 << endl;
+	
+	return 0;
+}
